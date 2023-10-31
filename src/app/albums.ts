0a714ebe5df2c8fd55fb004d42a9e53c80dcecf5
MuratYildirim95 <m.yildirim25@live.at>
2023-10-31 10:42:19 +0100

    asdf

---
+++ src/app/albums.ts
@@ -0,0 +1,20 @@
+
+
+export const albums = [
+    {
+        name: "Weddings",
+        image: "../assets/category/wedding/wedding.jpg",
+        description: "Delicious spaghetti with Bolognese sauce.",
+        pictures: [
+            {name: "Jean&John" ,picture:"../assets/category/wedding/Jean&John.jpg" , date: "01.01.2001"},
+            {name: "Jelly&Bean" ,picture:"../assets/category/wedding/Jelly&Bean.jpg" , date: "02.01.2002"}
+        ]
+    },{
+        name: "Partys",
+        image: "../assets/category/partys/partys.jpg",
+        description: "Parteeeey.",
+        pictures: [
+            {name: "WeirdParty" ,picture:"../assets/category/partys/weirdparty.jpg" , date: "01.01.2003"},
+            {name: "Jelly&Bean" ,picture:"../assets/category/partys/Jelly&Bean.jpg" , date: "02.01.2004"}
+        ]
+    }];
