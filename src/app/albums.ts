66e45b7bc72ab2a096c4472a29ddad7d432c9b5d
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-10 17:37:25 +0100

    fix path of images (assets/... instad of ../../assets/...)

--- src/app/albums.ts
+++ src/app/albums.ts
@@ -1,116 +1,116 @@
 export const weddings = [
   {
     id: 0,
-    image: '../assets/category/weddings/wed_0.jpg',
+    image: 'assets/category/weddings/wed_0.jpg',
   },
   {
     id: 1,
-    image: '../assets/category/weddings/wed_1.jpg',
+    image: 'assets/category/weddings/wed_1.jpg',
   },
   {
     id: 2,
-    image: '../assets/category/weddings/wed_2.jpg',
+    image: 'assets/category/weddings/wed_2.jpg',
   },
   {
     id: 3,
-    image: '../assets/category/weddings/wed_3.jpg',
+    image: 'assets/category/weddings/wed_3.jpg',
   },
   {
     id: 4,
-    image: '../assets/category/weddings/wed_4.jpg',
+    image: 'assets/category/weddings/wed_4.jpg',
   },
   {
     id: 5,
-    image: '../assets/category/weddings/wed_5.jpg',
+    image: 'assets/category/weddings/wed_5.jpg',
   },
   {
     id: 6,
-    image: '../assets/category/weddings/wed_6.jpg',
+    image: 'assets/category/weddings/wed_6.jpg',
   },
   {
     id: 7,
-    image: '../assets/category/weddings/wed_7.jpg',
+    image: 'assets/category/weddings/wed_7.jpg',
   },
   {
     id: 8,
-    image: '../assets/category/weddings/wed_8.jpg',
+    image: 'assets/category/weddings/wed_8.jpg',
   },
 ];
 
 export const portraits = [
   {
     id: 0,
-    image: '../assets/category/portraits/portrait_0.jpg',
+    image: 'assets/category/portraits/portrait_0.jpg',
   },
   {
     id: 1,
-    image: '../assets/category/portraits/portrait_1.jpg',
+    image: 'assets/category/portraits/portrait_1.jpg',
   },
   {
     id: 2,
-    image: '../assets/category/portraits/portrait_2.jpg',
+    image: 'assets/category/portraits/portrait_2.jpg',
   },
   {
     id: 3,
-    image: '../assets/category/portraits/portrait_3.jpg',
+    image: 'assets/category/portraits/portrait_3.jpg',
   },
   {
     id: 4,
-    image: '../assets/category/portraits/portrait_4.jpg',
+    image: 'assets/category/portraits/portrait_4.jpg',
   },
   {
     id: 5,
-    image: '../assets/category/portraits/portrait_5.jpg',
+    image: 'assets/category/portraits/portrait_5.jpg',
   },
   {
     id: 6,
-    image: '../assets/category/portraits/portrait_6.jpg',
+    image: 'assets/category/portraits/portrait_6.jpg',
   },
   {
     id: 7,
-    image: '../assets/category/portraits/portrait_7.jpg',
+    image: 'assets/category/portraits/portrait_7.jpg',
   },
   {
     id: 8,
-    image: '../assets/category/portraits/portrait_8.jpg',
+    image: 'assets/category/portraits/portrait_8.jpg',
   },
 ];
 
 export const retro_cars = [
   {
     id: 0,
-    image: '../assets/category/retro_cars/car_0.jpg',
+    image: 'assets/category/retro_cars/car_0.jpg',
   },
   {
     id: 1,
-    image: '../assets/category/retro_cars/car_1.jpg',
+    image: 'assets/category/retro_cars/car_1.jpg',
   },
   {
     id: 2,
-    image: '../assets/category/retro_cars/car_2.jpg',
+    image: 'assets/category/retro_cars/car_2.jpg',
   },
   {
     id: 3,
-    image: '../assets/category/retro_cars/car_3.jpg',
+    image: 'assets/category/retro_cars/car_3.jpg',
   },
   {
     id: 4,
-    image: '../assets/category/retro_cars/car_4.jpg',
+    image: 'assets/category/retro_cars/car_4.jpg',
   },
   {
     id: 5,
-    image: '../assets/category/retro_cars/car_5.jpg',
+    image: 'assets/category/retro_cars/car_5.jpg',
   },
   {
     id: 6,
-    image: '../assets/category/retro_cars/car_6.jpg',
+    image: 'assets/category/retro_cars/car_6.jpg',
   },
   {
     id: 7,
-    image: '../assets/category/retro_cars/car_7.jpg',
+    image: 'assets/category/retro_cars/car_7.jpg',
   },
   {
     id: 8,
-    image: '../assets/category/retro_cars/car_8.jpg',
+    image: 'assets/category/retro_cars/car_8.jpg',
   },
 ];
