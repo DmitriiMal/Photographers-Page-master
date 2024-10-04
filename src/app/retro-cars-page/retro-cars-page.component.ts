68b99efc90ddd04a5c4c945f55bebb60177bc176
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-04 12:22:41 +0200

    fix error in retro-cars-page.component.spec.ts

--- src/app/retro-cars-page/retro-cars-page.component.ts
+++ src/app/retro-cars-page/retro-cars-page.component.ts
@@ -9,38 +9,31 @@ import { Lightbox } from 'ngx-lightbox';
   styleUrls: ['./retro-cars-page.component.scss'],
 })
 export class RetroCarsPageComponent {
-  // retro_cars: Ialbum[] = retro_cars;
+  retro_cars: Ialbum[] = retro_cars;
 
+  // private _album: Array = [];
+  // constructor(private _lightbox: Lightbox) {
+  //   for (let i = 1; i <= 4; i++) {
+  //     const src = 'demo/img/image' + i + '.jpg';
+  //     const caption = 'Image ' + i + ' caption here';
+  //     const thumb = 'demo/img/image' + i + '-thumb.jpg';
+  //     const album = {
+  //        src: src,
+  //        caption: caption,
+  //        thumb: thumb
+  //     };
 
-  private _album: Array = [];
-  constructor(private _lightbox: Lightbox) {
-    for (let i = 1; i <= 4; i++) {
-      const src = 'demo/img/image' + i + '.jpg';
-      const caption = 'Image ' + i + ' caption here';
-      const thumb = 'demo/img/image' + i + '-thumb.jpg';
-      const album = {
-         src: src,
-         caption: caption,
-         thumb: thumb
-      };
+  //     this._albums.push(album);
+  //   }
 
-      this._albums.push(album);
-    }
+  //   open(index: number): void {
+  //     // open lightbox
+  //     this._lightbox.open(this._albums, index);
+  //   }
 
-    open(index: number): void {
-      // open lightbox
-      this._lightbox.open(this._albums, index);
-    }
-  
-    close(): void {
-      // close lightbox programmatically
-      this._lightbox.close();
-    }
-  }
-
-
-  
-
-
-
+  //   close(): void {
+  //     // close lightbox programmatically
+  //     this._lightbox.close();
+  //   }
+  // }
 }
